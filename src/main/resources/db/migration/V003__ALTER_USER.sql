fa0d2e0169abc22f30770369d42fbf34b50290ae
Dedrimer <13420015738@sina.cn>
2024-11-04 14:24:41 +0800

    修正SQL语句错误

--- src/main/resources/db/migration/V003__ALTER_USER.sql
+++ src/main/resources/db/migration/V003__ALTER_USER.sql
@@ -1,4 +1,4 @@
 ALTER table tab_user add column role varchar(50) not null comment '角色';
-ALTER TABLE tab_user;
-ADD COLUMN salt VARCHAR(8);
+ALTER TABLE tab_user,
+ADD COLUMN salt VARCHAR(8),
 MODIFY COLUMN password VARCHAR(128);
