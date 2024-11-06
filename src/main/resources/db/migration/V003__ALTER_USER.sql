b544c1b9b8ac2d23c614a345f33a560afc7c9523
Dedrimer <13420015738@sina.cn>
2024-11-06 09:48:04 +0800

    重新修改数据库脚本以确保密码加密工作正常

--- src/main/resources/db/migration/V003__ALTER_USER.sql
+++ src/main/resources/db/migration/V003__ALTER_USER.sql
@@ -1,4 +1,3 @@
 ALTER table tab_user add column role varchar(50) not null comment '角色';
-ALTER TABLE tab_user,
-ADD COLUMN salt VARCHAR(8),
-MODIFY COLUMN password VARCHAR(128);
+ALTER TABLE tab_user ADD COLUMN salt VARCHAR(8);
+ALTER TABLE tab_user MODIFY COLUMN password VARCHAR(128);
